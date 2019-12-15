fe5382988345cf377786c93c935477ee57214139
Julien TASSIN <julien.tassin.pro@gmail.com>
2019-12-15 17:45:53 +0100

    feat: add jira fetch

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,14 +2,17 @@
 // import index from '../src/index'
 
 import nock from 'nock'
+import delay from 'delay'
 // Requiring our app implementation
 import myProbotApp from '../src'
 import { Probot } from 'probot'
 // Requiring our fixtures
-import payload from './fixtures/issues.opened.json'
-const issueCreatedBody = { body: 'Thanks for opening this issue!' }
+import payload from './fixtures/create.branch.json'
+import JIRA_RESPONSE from './fixtures/jira.response'
 const fs = require('fs')
 const path = require('path')
+
+process.env.JIRA_URL = 'http://orga.atlassian.net'
 
 describe('My Probot app', () => {
   let probot: any
@@ -30,22 +33,55 @@ describe('My Probot app', () => {
     probot.load(myProbotApp)
   })
 
-  test('creates a comment when an issue is opened', async (done) => {
+  it('creates a pull request when a jira branch is created', async (done) => {
     // Test that we correctly return a test token
     nock('https://api.github.com')
       .post('/app/installations/2/access_tokens')
       .reply(200, { token: 'test' })
 
     // Test that a comment is posted
-    nock('https://api.github.com')
-      .post('/repos/hiimbex/testing-things/issues/1/comments', (body: any) => {
-        done(expect(body).toMatchObject(issueCreatedBody))
+    nock('http://orga.atlassian.net')
+      .post('/rest/api/3/search', (body: any) => {
+        expect(body).toMatchObject({
+          validateQuery: false,
+          jql: 'issueKey in (BO-1234)',
+          fieldsByKeys: true,
+          fields: ['issuetype', 'parent', 'summary']
+        })
         return true
       })
-      .reply(200)
+      .reply(200, JIRA_RESPONSE)
+
+    nock('https://api.github.com')
+      .post('/repos/Codertocat/Hello-World/pulls', (body: any) => {
+        done(expect(body).toMatchSnapshot())
+        return true
+      })
+      .reply(200, {})
 
     // Receive a webhook event
-    await probot.receive({ name: 'issues', payload })
+    await probot.receive({ name: 'create', payload })
+  })
+
+  it('does not create a pull request when a non jira branch is created', async (done) => {
+    // Test that we correctly return a test token
+    nock('https://api.github.com')
+      .post('/app/installations/2/access_tokens')
+      .reply(200, { token: 'test' })
+
+    // // Test that a comment is not posted
+    nock('http://orga.atlassian.net')
+      .post('/rest/api/3/search', (body: any) => {
+        done('should not be invoked')
+        return true
+      })
+      .reply(500)
+
+    // Receive a webhook event
+    await probot.receive({ name: 'create', payload: { ...payload, ref: 'non-jira' } })
+    await delay(200)
+    done()
+    // expect(nock.)
   })
 
   afterEach(() => {
